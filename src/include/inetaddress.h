c3f6a1506969580bc48557513f929b7472a1523d
ugovaretto <ugovaretto@gmail.com>
2016-08-18 12:30:04 +0200

    fix #14 it is now possible to extract client address on the server

--- src/include/inetaddress.h
+++ src/include/inetaddress.h
@@ -2,11 +2,13 @@
 //Functions for getting address through low level socket interface,
 //not working properly when retrieving socket file descriptor from
 //zeromq
+//Implemented pure UDP based communication: this works
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 #include <cstring>
 #include <stdexcept>
@@ -15,14 +17,121 @@
 #include <zmq.h>
 
 #include <string>
+#include <vector>
 
-
-struct AddrInfo {
-    int port;
-    std::string address;
-};
 
 namespace {
+
+//==============================================================================
+//send message to remote address and verify that received message is the
+//same as the one sent
+bool SendAddress(const char* hostname, int portno) {
+    int sockfd, n;
+    int serverlen;
+    struct sockaddr_in serveraddr;
+
+
+    // socket: create the socket
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+        throw std::runtime_error("ERROR opening socket");
+
+    // gethostbyname: get the server's DNS entry
+    struct hostent* server = gethostbyname(hostname);
+    if(!server) {
+        throw std::runtime_error(
+            "ERROR, no such host as " + std::string(hostname));
+    }
+
+    /* build the server's Internet address */
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    memcpy(&serveraddr.sin_addr.s_addr, server->h_addr, server->h_length);
+    serveraddr.sin_port = htons(portno);
+
+    std::vector< char > msg = {'h', 'e', 'l', 'l', 'o'};
+
+    // send the message to the server
+    serverlen = sizeof(serveraddr);
+    n = sendto(sockfd, &msg[0], msg.size(), 0,
+               (sockaddr*) &serveraddr, serverlen);
+    if (n < 0)
+        throw std::runtime_error("ERROR in sendto");
+
+    std::vector< char > reply(msg.size(), '\0');
+    /* print the server's reply */
+    n = recvfrom(sockfd, &reply[0], reply.size(), 0, (sockaddr*) &serveraddr,
+                 (socklen_t*) &serverlen);
+    if (n < 0)
+        throw std::runtime_error("ERROR in recvfrom");
+    shutdown(sockfd, 2);
+    return msg == reply;
+}
+
+//echo received message and return adress of sender
+//no ipv6 support for now
+std::string ReceiveAddress(int portno) {
+    struct sockaddr_in serveraddr;
+    struct sockaddr_in clientaddr;
+    struct hostent *hostp;
+
+    //socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0)
+        throw std::runtime_error("ERROR opening socket");
+
+    //reuse address
+    int optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+               (const void *)&optval , sizeof(int));
+
+
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    /*
+     * bind: associate the parent socket with a port
+     */
+    if(bind(sockfd, (struct sockaddr *) &serveraddr,
+             sizeof(serveraddr)) < 0)
+        throw std::runtime_error("ERROR on binding");
+
+    int clientlen = int(sizeof(clientaddr));
+    std::vector< char > msg(0x100, '\0');
+    int n = recvfrom(sockfd, &msg[0], msg.size(), 0,
+                     (struct sockaddr *) &clientaddr, (socklen_t*) &clientlen);
+    if (n < 0)
+        throw std::runtime_error("ERROR in recvfrom");
+
+    //retrieve sender's info
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+                          sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if(!hostp)
+        throw std::runtime_error("ERROR on gethostbyaddr");
+    const std::string hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if(!hostaddrp.size())
+        throw std::runtime_error("ERROR on inet_ntoas");
+
+    //echo received message
+    n = sendto(sockfd, &msg[0], msg.size(), 0,
+               (struct sockaddr *) &clientaddr, clientlen);
+    if (n < 0)
+        throw std::runtime_error("ERROR in sendto");
+
+    //shutdown
+    shutdown(sockfd, 2);
+    return hostaddrp;
+}
+
+//==============================================================================
+struct AddrInfo {
+    int port;
+    std::vector< char > address;
+};
+
+
 AddrInfo PeerAddress(int s) {
     struct sockaddr_storage addr;
     socklen_t len = sizeof(addr);
@@ -41,21 +150,21 @@ AddrInfo PeerAddress(int s) {
         port = ntohs(s->sin6_port);
         inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof(ipstr));
     }
-    return {port, std::string(ipstr)};
+    return {port, std::vector< char >(ipstr, ipstr + sizeof(ipstr))};
 }
 
-std::string PeerZMQAddressURI(void* zmqSocket) {
-    if(!zmqSocket)
-        throw std::runtime_error("NULL ZMQ SOCKET");
-    int fd = 0;
-    size_t fdsz = sizeof(fd);
-    zmq_getsockopt(zmqSocket, ZMQ_FD, &fd, &fdsz);
-    AddrInfo ai = PeerAddress(fd);
-    return std::string("tcp://")
-        + ai.address
-        + std::string(":")
-        + std::to_string(ai.port);
-}
+//std::string PeerZMQAddressURI(void* zmqSocket) {
+//    if(!zmqSocket)
+//        throw std::runtime_error("NULL ZMQ SOCKET");
+//    int fd = 0;
+//    size_t fdsz = sizeof(fd);
+//    zmq_getsockopt(zmqSocket, ZMQ_FD, &fd, &fdsz);
+//    AddrInfo ai = PeerAddress(fd);
+//    return std::string("tcp://")
+//        + ai.address
+//        + std::string(":")
+//        + std::to_string(ai.port);
+//}
 
 AddrInfo PeerZMQAddress(void* zmqSocket) {
     if(!zmqSocket)
@@ -83,21 +192,21 @@ AddrInfo LocalAddress(int s) {
         port = ntohs(s->sin6_port);
         inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
     }
-    return {port, std::string(ipstr)};
+    return {port, std::vector< char >(ipstr, ipstr + sizeof(ipstr))};
 }
 
-std::string LocalZMQAddressURI(void* zmqSocket) {
-    if(!zmqSocket)
-        throw std::runtime_error("NULL ZMQ SOCKET");
-    int64_t fd;
-    size_t fdsz;
-    zmq_getsockopt(zmqSocket, ZMQ_FD, &fd, &fdsz);
-    AddrInfo ai = LocalAddress(fd);
-    return std::string("tcp://")
-        + ai.address
-        + std::string(":")
-        + std::to_string(ai.port);
-}
+//std::string LocalZMQAddressURI(void* zmqSocket) {
+//    if(!zmqSocket)
+//        throw std::runtime_error("NULL ZMQ SOCKET");
+//    int64_t fd;
+//    size_t fdsz;
+//    zmq_getsockopt(zmqSocket, ZMQ_FD, &fd, &fdsz);
+//    AddrInfo ai = LocalAddress(fd);
+//    return std::string("tcp://")
+//        + ai.address
+//        + std::string(":")
+//        + std::to_string(ai.port);
+//}
 
 AddrInfo LocalZMQAddress(void* zmqSocket) {
     if(!zmqSocket)
---
+++ src/test/peernametest.cpp
@@ -0,0 +1,59 @@
+//Author: Ugo Varetto
+//
+// This file is part of zrf - zeromq remoting framework.
+//zrf is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//zrf is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with zrf.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <cassert>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "inetaddress.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    const bool isServer = std::find_if(argv, argv + argc,
+                                       [](const char* arg) {
+                                           return string(arg) == "-server";})
+        != argv + argc;
+
+    const char* URI = isServer ? "tcp://*:6667" : "tcp://localhost:6667";
+    void* ctx = zmq_ctx_new();
+    void* s = isServer ? zmq_socket(ctx, ZMQ_REP) : zmq_socket(ctx, ZMQ_REQ);
+    vector< char > buffer(0x100);
+    if(isServer) {
+        assert(ReceiveAddress(6667) == "tcp://localhost:6667");
+        zmq_bind(s, URI);
+        while(true) {
+            assert(zmq_recv(s, &buffer[0], buffer.size(), 0) >= 0);
+            AddrInfo ai = PeerZMQAddress(s);
+            for(auto i: ai.address)
+                cout << i << ' ';
+            cout << endl;
+            zmq_send(s, &buffer[0], buffer.size(), 0);
+        }
+    } else {
+        assert(SendAddress("127.0.0.1", 6667));
+        zmq_connect(s, URI);
+        zmq_send(s, &buffer[0], buffer.size(), 0);
+        assert(zmq_recv(s, &buffer[0], buffer.size(), 0) >= 0);
+    }
+    zmq_close(s);
+    zmq_ctx_destroy(ctx);
+    return EXIT_SUCCESS;
+}
+
+
