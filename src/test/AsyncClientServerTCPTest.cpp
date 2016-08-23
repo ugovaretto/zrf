1360bc4332134b306a1a89edbfe06d56446ada67
Tim <tdykes@cray.com>
2016-08-23 04:41:38 -0500

    Minimum reproducer of issue 28

--- src/test/AsyncClientServerTCPTest.cpp
+++ src/test/AsyncClientServerTCPTest.cpp
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
                                            return string(arg) == "-server";})
         != argv + argc;
 
-    const char* URI = isServer ? "tcp://*:6667" : "tcp://localhost:6667";
+    const char* URI = isServer ? "tcp://*:5556" : "tcp://localhost:5556";
     using Client = AsyncClient<>;
     using Server = AsyncServer<>;
     using Rep = Client::ReplyType;
@@ -62,35 +62,42 @@ int main(int argc, char** argv) {
     if(isServer) {
         //SERVER
         Server server;
+        printf("Server running\n");
         //server receives a service object used to service client requests
         auto service = [](const ByteArray& req) {
-            string s = UnPack< string >(req);
-            Reverse(s.begin(), s.end());
+            std::tuple<int,int,int> tpe; 
+            UnPack(begin(req),tpe);
+            printf("Server Recieved tuple: %d, %d, %d\n", std::get<0>(tpe), std::get<1>(tpe), std::get<2>(tpe));
+            fflush(0);
+            std::string s = "return";
             return Pack(s);
         };
         server.Start(URI, service);
     } else {
         Client client(URI);
-        const string reqString = "hello";
+        const std::tuple<int, int, int> tpe = std::make_tuple(0,1,2);
+       // const string reqString = "hello";
         //create reference data for validation purposes
-        string refRepString = Reverse(reqString);
+       // string refRepString = Reverse(reqString);
         //create byte array to send...
         //ByteArray req = Pack(reqString);
         //asynchronously send request
         //Rep rep = client.Send(req);
         //...or just invoke SendArgs directly with any number of arguments for which
         //a serializer specialization is available
-        Rep rep = client.SendArgs(reqString);
+        printf("Client sending tuple: %d, %d, %d\n", std::get<0>(tpe), std::get<1>(tpe), std::get<2>(tpe));
+        fflush(0);
+        Rep rep = client.SendArgs(tpe);
         //Rep encapsulates a sent request and will block on .Get() waiting
         //to receive a response
         //when assigning directly to a type T .Get() gets invoked automatically
         //through operator T()
         //receive, extract and return reply
-        string repString = rep; //equivalent to UnPack< string >(rep.Get());
-
+        std::string repString = rep; //equivalent to UnPack< string >(rep.Get());
+        printf("Rep: %s\n", repString.c_str());
         //VALIDATE
-        assert(repString == refRepString);
-        cout << "PASSED" << endl;
+        //assert(repString == refRepString);
+        //cout << "PASSED" << endl;
     }
     return EXIT_SUCCESS;
 }
